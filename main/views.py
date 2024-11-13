0d78a39128b5e6a32602cb0ac4b9618017daffb0
Igor <qwer@gmail.com>
2024-11-13 04:37:21 +0300

    add two views

--- main/views.py
+++ main/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from models import Planet, Category
 
-# Create your views here.
+
+def popular_list(request):
+    planets = Planet.objects.filter(available=True)[:3]
+    return render(request, 
+                  'main/index/index.html', 
+                  {'planets': planets})
+
+def planet_detail(request, planet_slug):
+    planet = get_object_or_404(Planet, 
+                               slug=planet_slug, 
+                               available=True)
+    return render(request, 
+                  'main/product/detail.html',
+                  {'planet':planet})
+
