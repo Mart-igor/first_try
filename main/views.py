f4d557ab39516445e79b6e37b5876db402769bfe
Igor <martynov.igor2020ty@gmail.com>
2024-11-14 21:00:35 +0300

    add paginator

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from models import Planet, Category
+from django.core.paginator import Paginator
 
 
 def popular_list(request):
@@ -17,13 +18,18 @@ def planet_detail(request, planet_slug):
                   {'planet':planet})
 
 def planet_list(request, category_slug=None):
+    page = request.GET.get('page', 1)
     category = None
     categories = Category.objects.all()
     planets = Planet.objects.filter(available=True)
+    paginator = Paginator(planets, 1)
+    page_obj = paginator.page(int(page))
     if category_slug:
         category = get_object_or_404(Category, 
                                      slug=category_slug)
         planets = Planet.objects.filter(category=category)
+        paginator = Paginator(planets, 1)
+        page_obj = paginator.page(int(page))
     return render(request, 'main/product/list.html', {'category':category,
                                                       'categories': categories,
                                                       'planets': planets})
